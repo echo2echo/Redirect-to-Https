471fc0a21ddfdb3bc8b9fb2ec149c795a22d90eb
Joel Rader <echophotographic@gmail.com>
2021-05-23 23:02:27 -0400

    Update redirectToHttps.js
    removed comments...  just a quick script

--- redirectToHttps.js
+++ redirectToHttps.js
@@ -1,11 +1,3 @@
-/* Script to redirect browser from http to https
-
-To reference this javascript file use the following in the <head> of your webpages
-
-<script src="./redirectToHttps.js"></script> 
-
-*/
-
 function redirectToHttps() {
         if (window.location.protocol != "https:") {
             var oldURL = window.location.hostname + window.location.pathname + window.location.search + window.location.hash;
